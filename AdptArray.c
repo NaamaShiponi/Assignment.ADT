5a3b89798914fce35d557a655ff820c85955f328
Naama Shiponi <naamas1234567@gmail.com>
2023-03-16 14:30:24 +0200

    add comment to AdptArray.c

--- AdptArray.c
+++ AdptArray.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include "AdptArray.h"
 #include <string.h>
 #include <stdlib.h>
+#include "AdptArray.h"
+
 
 typedef struct AdptArray_{
 	PElement* pE;
@@ -13,19 +14,18 @@ typedef struct AdptArray_{
 
 
 PAdptArray CreateAdptArray(COPY_FUNC  f_copy, DEL_FUNC f_del,PRINT_FUNC f_print){
-  
+    
+    //Finding space for the new AdptArray_ struct
     PAdptArray pNewArr = (PAdptArray)malloc(sizeof(AdptArray));
-  
+    
+    //Check that malloc worked
     if(pNewArr==NULL) return NULL;
-   
+    
+    //Insert parameters to struct AdptArray_
     pNewArr->pE=NULL;
-   
     pNewArr->f_copy=f_copy;
-   
     pNewArr->f_del=f_del;
-   
     pNewArr->f_print=f_print;
-   
     pNewArr->arrSize=0;
    
     return pNewArr;
@@ -33,79 +33,92 @@ PAdptArray CreateAdptArray(COPY_FUNC  f_copy, DEL_FUNC f_del,PRINT_FUNC f_print)
 
 
 void DeleteAdptArray(PAdptArray pArr){
-  
-    if(pArr!=NULL){
+    
+    if(pArr!=NULL){ //Check that pArr exists
   
         for(int i=0; i<(pArr->arrSize); i++){
   
-            if((pArr->pE)[i] != NULL){
+            if((pArr->pE)[i] != NULL){ //Check that there is an element in the "i" position in the array
   
-                pArr->f_del((pArr->pE)[i]);
+                //Delete the element with the delete function we received
+                pArr->f_del((pArr->pE)[i]); 
   
             }
   
         }
-  
+        //Free the arr space
         free(pArr->pE);
-  
+        
+        //Free struct AdptArray_
         free(pArr);
   
     }
 }
 
 
+
 Result SetAdptArrayAt(PAdptArray pArr, int i, PElement pe){
     
+    //Check that pArr exists
     if(pArr==NULL) return FAIL;
     
+    //Create a new pointer to AdptArray_
     PElement newPArr;
 
-    //לבדוק שהאינדקס זה בגודל המערך
+    //If the index we received is greater than the size of the array
     if(i < 0 || pArr->arrSize<=i){
 
+        //Finding space for the new array
         newPArr = (PElement*)calloc((i + 1), sizeof(PElement));
 
+        //Check that calloc worked
         if(newPArr == NULL) return FAIL;
         
+        //Copy all the elements from the old array to the new one
 		memcpy(newPArr, pArr->pE, (pArr->arrSize) * sizeof(PElement));
 
+        //Free the old array
         free(pArr->pE);
 		
+        //Update the array in struct AdptArray_
         pArr->pE = newPArr;
         
+        //Update the size of the array in struct AdptArray_
         pArr->arrSize=i+1;
     }
    
-    //לשחרר אם יש שם משהו כבר 
-    if((pArr->pE)[i]!=NULL){
+    if((pArr->pE)[i]!=NULL){ //Check if there is an element in the "i" place
     
+        //Free the memory in place "i"
         pArr->f_del((pArr->pE)[i]);
     
     }
-   
+    
+    //Create a copy of the element and place it in the array
     (pArr->pE)[i]=pArr->f_copy(pe);
    
     return SUCCESS;
 }
 
 
+
 PElement GetAdptArrayAt(PAdptArray pArr, int i){
     
+    //Check that pArr exists and the element in place "i" exists
     if(pArr== NULL || pArr->arrSize < i || (pArr->pE)[i]==NULL) return NULL;  
     
+    //Return the element
     return pArr->f_copy((pArr->pE)[i]);
 
 }
 
 
 int GetAdptArraySize(PAdptArray pArr){
+    
+    //Check that pArr exists
+    if(pArr==NULL) return -1;
 
-    if(pArr==NULL){
-
-        return -1;
-
-    }
-
+    //Return the array size
     return pArr->arrSize;
 
 }
@@ -113,10 +126,12 @@ int GetAdptArraySize(PAdptArray pArr){
 
 void PrintDB(PAdptArray pArr){
 
-    if(pArr!=NULL){
+    if(pArr!=NULL){ //Check that pArr exists
 
+        //Go through all the elements in the array
         for(int i=0;i<pArr->arrSize;i++){
 
+            //If the element exists we will print it
             if((pArr->pE)[i]!=NULL) pArr->f_print((pArr->pE)[i]);
 
         }
